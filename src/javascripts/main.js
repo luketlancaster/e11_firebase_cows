af32885911b14e7af77165f814050a700d507576
Luke Lancaster <luketlancaster@gmail.com>
2020-03-21 11:15:11 -0500

    Create logout button event

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 
 import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
 import authData from './helpers/data/authData';
 
 import apiKeys from './helpers/apiKeys.json';
@@ -12,6 +13,7 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logOutEvent();
 };
 
 init();
